9f2f982815df8d14cbdb3125ef8190a887b9a0dd
jkang005 <jkang005@gold.ac.uk>
2022-06-10 02:26:26 +0100

    map under working

--- src/map.java
+++ src/map.java
@@ -1,9 +1,13 @@
-//public class map {
-//
-////import google map api key AIzaSyDh4O2xnh9pmw4RAO7sOpVwlzKtHgQQY0I
-//    public static String mapKey = "AIzaSyDh4O2xnh9pmw4RAO7sOpVwlzKtHgQQY0I";
-//    //calculate the distance between two addresses
-//
-//
-//
-//}
+import java.awt.*;
+
+public class map {
+    OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+    PageAttributes.MediaType mediaType = PageAttributes.MediaType.parse("text/plain");
+    RequestBody body = RequestBody.create(mediaType, "");
+    Request request = new Request.Builder()
+            .url("https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=YOUR_API_KEY")
+            .method("GET", body)
+            .build();
+    Response response = client.newCall(request).execute();
+}
